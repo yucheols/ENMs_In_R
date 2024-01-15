41d4840b42da02610f0828daa3d374cb99d01db8
Yucheol Shin <brongersmai2@gmail.com>
2024-01-15 19:53:46 +0900

    updateCodes

--- code/tutorial_code.R
+++ code/tutorial_code.R
@@ -10,7 +10,6 @@
 #######################################################  The workflow starts here  ######################################################
 ## load packages
 library(terra)
-library(raster)
 library(dplyr)
 library(SDMtune)
 library(ENMeval)
@@ -25,3 +24,38 @@ library(ggplot2)
 #dir.create('env_processed') # to contain the processed raster layers
 
 #####  PART 1 ::: load and prepare environmental data  #####
+### first, we will import a polygon file for the Korean Peninsula to process our environmental layers
+poly <- vect('poly/kor_mer.shp')
+
+### now we will import, crop, and mask our layers
+# climate
+clim <- rast(list.files(path = 'climate', pattern = '.tif$', full.names = T))  # import
+clim <- crop(clim, poly)                            # crop == crop to geographic extent
+clim <- mask(clim, poly)                            # mask == cut along the polygon boundary ("cookie cutter")
+
+plot(clim[[1]]) # It is always a good idea to plot out the processed layer(s)
+
+# topo
+topo <- rast(list.files(path = 'topo', pattern = '.tif$', full.names = T))
+topo <- crop(topo, poly)
+topo <- mask(topo, poly)
+
+# land cover
+land <- rast('land/mixed_other.tif')
+land <- crop(land, poly)
+land <- mask(land, poly)
+
+### stack into one object
+envs <- rast(c(clim, topo, land))
+print(envs)
+
+### optional ::: you can choose to export the processed layers
+
+
+#####  PART 2 ::: occurrence data  #####
+# there are several ways to extract the occurrence data. But here we will use the megaSDM package to quickly scrape the 
+# data from GBIF. NOTE: you may need to install this package. Refer to the following link for instructions for installation:
+# https://github.com/brshipley/megaSDM
+
+megaSDM::OccurrenceCollection(spplist = c('Bufo stejnegeri'), output = 'occs',
+                              trainingarea = c(124.1833, 130.9417, 33.10833, 43.00833))
