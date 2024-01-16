a59809db19c7d4c0bf64f716eaa99fbf1a2dc83a
Yucheol Shin <brongersmai2@gmail.com>
2024-01-16 16:06:57 +0900

    update tutorial code

--- code/tutorial_code.R
+++ code/tutorial_code.R
@@ -10,6 +10,7 @@
 #######################################################  The workflow starts here  ######################################################
 ## load packages
 library(terra)
+library(raster)
 library(dplyr)
 library(SDMtune)
 library(ENMeval)
@@ -17,7 +18,13 @@ library(extrafont)
 library(rasterVis)
 library(ggplot2)
 
-## Other than the packages above, make sure to have "humboldt" package installed 
+## Other than the packages above, make sure to have "humboldt", "sf" packages installed .
+
+## Note on the usage of the "raster" package ::: Since SDMtune accepts SpatRaster objects created from the terra package, 
+## we really don't need to rely on the raster package. But the exclusive use of terra package is producing some weird errors 
+## on my computer that I cannot fully solve. So I'm mainly using the raster package here for raster data processing and will 
+## use the terra package for formatting data into SDMtune inputs. 
+
 
 ## run the codes below ONCE ONLY == after running them once, lock them with "#" so that they are recognized as comments, not codes 
 #dir.create('climate')
@@ -27,33 +34,37 @@ library(ggplot2)
 
 #####  PART 1 ::: load and prepare environmental data  #####
 ### first, we will import a polygon file for the Korean Peninsula to process our environmental layers
-poly <- vect('poly/kor_mer.shp')
+poly <- sf::st_read('poly/kor_mer.shp')
 
 ### now we will import, crop, and mask our layers
 # climate
-clim <- rast(list.files(path = 'climate', pattern = '.tif$', full.names = T))  # import
-clim <- crop(clim, poly)                            # crop == crop to geographic extent
+clim <- raster::stack(list.files(path = 'climate', pattern = '.tif$', full.names = T))  # import
+clim <- raster::crop(clim, extent(poly))                            # crop == crop to geographic extent
 clim <- mask(clim, poly)                            # mask == cut along the polygon boundary ("cookie cutter")
 
 plot(clim[[1]]) # It is always a good idea to plot out the processed layer(s)
 
 # topo
-topo <- rast(list.files(path = 'topo', pattern = '.tif$', full.names = T))
-topo <- crop(topo, poly)
+topo <- raster::stack(list.files(path = 'topo', pattern = '.tif$', full.names = T))
+topo <- crop(topo, extent(poly))
 topo <- mask(topo, poly)
 
 # land cover
-land <- rast('land/mixed_other.tif')
-land <- crop(land, poly)
+land <- raster('land/mixed_other.tif')
+land <- crop(land, extent(poly))
 land <- mask(land, poly)
 
 ### stack into one object == use "c()" for the terra equivalent of the "raster::stack()", which is for the raster package
-envs <- c(clim, topo, land)
+envs <- raster::stack(clim, topo, land)
 print(envs)
 plot(envs[[1]])
 
 ### optional ::: you can choose to export the processed layers
-
+for (i in 1:nlayers(envs)) {
+  layer <- envs[[i]]
+  file_name <- paste0('env_processed/', names(envs)[i], '.tif')
+  writeRaster(layer, filename = file_name, overwrite = T)
+}
 
 
 #####  PART 2 ::: occurrence data  #####
@@ -63,7 +74,7 @@ plot(envs[[1]])
 
 # collect occurrence points
 megaSDM::OccurrenceCollection(spplist = c('Bufo stejnegeri'), output = 'occs',
-                              trainingarea = c(124.1833, 130.9417, 33.10833, 43.00833))
+                              trainingarea = extent(envs[[1]]))
 
 # now lets look at the data
 occs <- read.csv('occs/Bufo_stejnegeri.csv')
@@ -72,12 +83,20 @@ head(occs)
 # since we only need the species name, longitude, and latitude, we will pull out those three columns only
 occs <- occs[, c('species', 'decimalLongitude', 'decimalLatitude')]
 colnames(occs) = c('species', 'long', 'lat')
+head(occs)
 
 # spatially thin the occurrence points. We can use the internal SDMtune function "thinData()", but this method matches the thinning
-# distance for
-occs <- thinData(coords = occs[, c(2,3)], env = envs, x = 'long', y = 'lat', verbose = T, progress = T)
+# distance to the spatial resolution of input raster layers. So, we may want to have more freedom with the selection of thinning
+# distance. For that we can use the "humboldt.occ.rarefy()" function in the humboldt package. Also check out the "spThin" package
 
-# plot out 
+# since out raw occurrence has > 600 occurrence points, lets try 10km thinning distance. This will thin down our data
+# to 215 point
+occs <- humboldt::humboldt.occ.rarefy(in.pts = occs, colxy = 2:3, rarefy.dist = 10, rarefy.units = 'km', run.silent.rar = F)
+
+# as with the raster data, it is always a good idea to plot out the occurrence points on the map 
+plot(envs[[1]])
+points(occs[, c(2,3)])
+
 
 #####  PART 3 ::: background point sampling  #####
 # you can sample your backgrounds in several different ways. We will first use random sampling 
